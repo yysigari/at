bef8eef3da276a068a0f37205c2958501955ce85
Laurent S. NADOLSKI <nadolski@synchrotron-soleil.fr>
2018-03-10 23:07:22 +0100

    First pages commit

--- atmat/atpath.m
+++ atmat/atpath.m
@@ -4,9 +4,12 @@ function atpath(varargin)
 ATROOT = fileparts(mfilename('fullpath'));
 ATINTEGRATORS = fullfile(fileparts(ATROOT),'atintegrators');
 MACHINEDATA = fullfile(fileparts(ATROOT),'machine_data');
+M2HTML = fullfile(fileparts(ATROOT),'docs', 'm2html');
 disp(ATROOT)
 addpath(genpath(ATROOT));
 disp(ATINTEGRATORS)
 addpath(genpath(ATINTEGRATORS));
 disp(MACHINEDATA)
 addpath(genpath(MACHINEDATA));
+disp(M2HTML)
+addpath(genpath(M2HTML));
