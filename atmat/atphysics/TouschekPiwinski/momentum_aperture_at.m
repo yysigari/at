4c74864607a8501d2b23857904b1d116034326f9
Laurent S. NADOLSKI <laurent.nadolski@synchrotron-soleil.fr>
2019-12-09 13:57:26 +0100

    update doc + convert to unix file

--- atmat/atphysics/TouschekPiwinski/momentum_aperture_at.m
+++ atmat/atphysics/TouschekPiwinski/momentum_aperture_at.m
@@ -1,28 +1,36 @@
-function [deltamax]=momentum_aperture_at(THERING,deltalimit,initcoord,delta,precdelta,deltastepsize,splits,split_step_divisor,nturns)
-% function [deltamin, deltamax...
-%     ]=momentum_aperture_at(THERING,...
-%     deltalimit,... [min max]
-%     initcoord,... [x y] initial coordinate
-%     delta,...
-%     precdelta,...
-%     deltastepsize,...
-%     splits,... % number of splitting
-%     split_step_divisor)  % divide the step size at every split
+function deltamax = momentum_aperture_at(THERING,deltalimit,initcoord,delta,precdelta,deltastepsize,splits,split_step_divisor,nturns)
+% momentum_aperture_at - computes half momentum acceptance of a ring
 %
-% following the ELEGANT routine:
-% Start with ? = 0, i.e., zero momentum offset.
-% 2. Track a particle to see if it gets lost. If so, proceed to step 4.
-% 3. Increase ? by step size ?? and return to step 2.
-% 4. If no splitting steps remain, proceed to the next step. Otherwise:
-% (a) Change ? to deltas ? sb??., where ?s is the largest ? for which the particle survived, and sb is the steps_back parameter.
-% (b) Divide the step size by split_step_divisor to get a new step size ??.
-% (c) Set?=?+??.
-% (d) Decrement the ?splits remaining? counter by 1.
-% (e) Continue from step 2.
-% 5. Stop. The momentum aperture is ?s
+%  INPUTS
+%  1. THERING - Ring structure
+%  2. deltalimit  [min max] - energy offset vairation for the search procedure
+%  3. initcoord [x y]       - initial coordinate for tracking, should be larger
+%  than 0
+%  4. delta                 - starting energy offset for search
+%  5. precdelta             -
+%  6. deltastepsize         -
+%  7. splits                - number of splitting
+%  8. split_step_divisor    - divide the step size at every split
 %
-% ex: [deltamax]=momentum_aperture_at(THERING,0.1,[10^-6 10^-6],0,0,0.01,3,10,100)
-% ex: [deltamin]=momentum_aperture_at(THERING,-0.1,[10^-6 10^-6],0,0,-0.01,3,10,100)
+%  OUTPUTS
+%  1. deltamax - vector of momentum acceptance
+%
+%  NOTES
+%  following the ELEGANT routine:
+%  Start with ? = 0, i.e., zero momentum offset.
+%  2. Track a particle to see if it gets lost. If so, proceed to step 4.
+%  3. Increase ? by step size ?? and return to step 2.
+%  4. If no splitting steps remain, proceed to the next step. Otherwise:
+%  (a) Change ? to deltas ? sb??., where ?s is the largest ? for which the particle survived, and sb is the steps_back parameter.
+%  (b) Divide the step size by split_step_divisor to get a new step size ??.
+%  (c) Set?=?+??.
+%  (d) Decrement the ?splits remaining? counter by 1.
+%  (e) Continue from step 2.
+%  5. Stop. The momentum aperture is ?s
+%
+%  EXAMPLES
+%  1. deltamax = momentum_aperture_at(THERING,0.1,[10^-6 10^-6],0,0,0.01,3,10,100)
+%  2. deltamin = momentum_aperture_at(THERING,-0.1,[10^-6 10^-6],0,0,-0.01,3,10,100)
 
 %disp([delta splits])
 
@@ -62,7 +70,6 @@ if ( delta>=0 && delta<deltalimit) ||  ( delta<=0 && delta>deltalimit)
             
         end
     else
-        
         % no splitting steps remain
         deltamax=delta-deltastepsize;
         
