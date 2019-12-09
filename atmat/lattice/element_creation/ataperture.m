65f7a3b273be2cd9792945b23beb6e13726614e8
Laurent S. NADOLSKI <laurent.nadolski@synchrotron-soleil.fr>
2019-12-09 13:57:48 +0100

    update doc

--- atmat/lattice/element_creation/ataperture.m
+++ atmat/lattice/element_creation/ataperture.m
@@ -1,18 +1,19 @@
 function elem=ataperture(fname,varargin)
-%ATAPERTURE Creates a aperture element
+%ATAPERTURE - Creates an aperture element
 % ATAPERTURE(FAMNAME,LIMITS,PASSMETHOD
 % define physical aperture element (collimator)
 % lim=[-x,+x,-y,+y];
-% 
+%
 % lim={[-x,+x,-y,+y],[-x,+x,-y,+y],[-x,+x,-y,+y],...};
 % will generate various aperture elements (one for every set of errors)
-% 
-%  See also setphysicalaperture
-% 
+%
+%  See Also plotAperture, PlotRAperture, PlotERAperture, SetPhysicalAperture
+%
 
 [rsrc,limits,method,~]=decodeatargs({[0 0],'AperturePass',''},varargin);
 [limits,rsrc]=getoption(rsrc,'Limits',limits);
 [method,rsrc]=getoption(rsrc,'PassMethod',method);
 [cl,rsrc]=getoption(rsrc,'Class','Aperture');
 elem=atbaselem(fname,method,'Class',cl,'Limits',limits,rsrc{:});
+
 end
