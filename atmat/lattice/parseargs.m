47b3b165ff78c911d31409397db71c5687ae7f28
Laurent S. NADOLSKI <laurent.nadolski@synchrotron-soleil.fr>
2019-12-09 12:03:38 +0100

    update documentation

--- atmat/lattice/parseargs.m
+++ atmat/lattice/parseargs.m
@@ -1,7 +1,13 @@
 function varargout = parseargs(default_values,args)
-%PARSEARGS Check and expands optional argument lists
-%ARGOUT=PARSEARGS(DEFAULT_VALUES,ARGIN)
-%[ARG1,ARG2,...]=PARSEARGS(DEFAULT_VALUES,ARSIN)
+%  parseargs - Check and expands optional argument lists
+%
+%  INPUTS
+%  1. default_values - Default value is not put in input argument
+%  2. args           - optional argument
+%
+%  OUTPUTS
+%  1. ARGOUT - PARSEARGS(DEFAULT_VALUES,ARGIN)
+%  2. [ARG1,ARG2,...]=PARSEARGS(DEFAULT_VALUES,ARSIN)
 
 na=min(length(default_values),length(args));
 ok=~cellfun(@(arg) isempty(arg)&&isnumeric(arg),args(1:na));
