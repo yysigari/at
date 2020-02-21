b26a2f321940012924519dcc2d672fc0235c976b
Laurent S. NADOLSKI <laurent.nadolski@synchrotron-soleil.fr>
2020-02-21 21:24:08 +0100

    add documentation

--- atmat/atdisplay.m
+++ atmat/atdisplay.m
@@ -1,12 +1,16 @@
 function atdisplay(verbosity,message)
-%ATDISPLAY checks the verbosity level in the global variable GLOBVAL
-%          and displays message if this is greater than the verbosity
-%          for this message.
+%ATDISPLAY - checks the verbosity level in the global variable GLOBVAL
+%            and displays message if this is greater than the verbosity
+%            for this message.
+% 
+%  INPUTS
+%  1. verbosity - level of verbosity
+%  2. message   - message du display
 %
-%  See also NUMDIFPARAMS
+%  See also numdifparams
 
 global GLOBVAL
 
-if isfield(GLOBVAL,'verbosity')&& verbosity<= GLOBVAL.verbosity
+if isfield(GLOBVAL,'verbosity') && verbosity<= GLOBVAL.verbosity
     disp(message)
 end
