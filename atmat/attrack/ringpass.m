7c75057da7a4c8c12159e5f8d7c0373a93c3e74f
Laurent S. NADOLSKI <laurent.nadolski@synchrotron-soleil.fr>
2020-02-21 21:27:11 +0100

    add doc

--- atmat/attrack/ringpass.m
+++ atmat/attrack/ringpass.m
@@ -1,99 +1,111 @@
-function [Rout, varargout] = ringpass(ring, Rin, nturns, varargin)
-%RINGPASS tracks particles through each element of the cell array RING
-% calling the element-specific tracking function specified in the
-% RING{i}.PassMethod field.
+function [ROUT, varargout] = ringpass(ring, RIN, NTURNS, varargin)
+%ringpass - tracks particles through each element of the cell array ring
+%  calling the element-specific tracking function specified in the
+%  ring{i}.PassMethod field.
 %
-% ROUT=RINGPASS(RING,RIN,NTURNS) tracks particle(s) with initial
+%  ROUT=ringpass(ring,RIN,NTURNS) tracks particle(s) with initial
 %    condition(s) RIN for NTURNS turns
 %
-%   RING        AT lattice
-%   RIN         6xN matrix: input coordinates of N particles
-%   NTURNS      Number of turns to perform (default: 1)
+%  [ROUT, LOST]=ringpass(...)
+%    Returns additionally an information on lost particles
 %
-%   ROUT        6x(N*NTURNS) matrix: output coordinates of N particles at
-%               the exit of each turn
+%  [ROUT, LOST, LASTTURN]=ringpass(...)
 %
-% [ROUT, LOST]=RINGPASS(...)
-%  Return additionally an information on lost particles
-%    LOST	1xN logical vector, indicating lost particles
-%    If only one output is given, loss information is saved in
-%    global variable LOSSFLAG
+%  [ROUT, LOSS, NTURNS, LOSSINFO]=ringpass(...,'nhist',NHIST,...)
 %
-% [ROUT, LOST, NTURNS]=RINGPASS(...)
-%  Return additionally the number of turns performed by each particle
-%	NTURNS	1xN vector, number of turns performed
+%  ROUT = ringpass(...,'KeepLattice') Tracking with the 'KeepLattice' flag is
+%     more efficient because it reuses persistent data structures stored in
+%     memory in previous calls to ringpass.
 %
-% [ROUT, LOSS, NTURNS, LOSSINFO]=RINGPASS(...,'nhist',NHIST,...)
-%  Return additional information on lost particles
-%   NHIST       number elements before the loss to be traced (default: 1)
-%   LOSSINFO	1x1 structure with the following fields:
-%               lost                 1xN logical vector, indicating lost particles
-%               turn                 1xN vector, turn number where the particle is lost
-%               element              1xN vector, element number where the particle is lost
-%               coordinates_at_loss  6xN array, coordinates at the exit of
-%                                    the element where the particle is lost
-%                                    (sixth coordinate is inf if particle is lost in a physical aperture)
-%               coordinates          6xNxNHIST array, coordinates at the entrance of the
-%                                    NHIST elements before the loss
+%  ROUT=ringpass(...,'reuse') is kept for compatibilty with previous
+%      versions. It has no effect.
+%  
+%  ROUT=ringpass(...,PREFUNC)
+%  ROUT=ringpass(...,PREFUNC,POSTFUNC)
+%  ROUT=ringpass(...,function_handle.empty,POSTFUNC)
+%     PREFUNC and POSTFUNC are function handles, PREFUNC is called
+%     immediately before tracking each element, POSTFUNC is called
+%     immediately after each element. Functions are called as:
+%       ROUT=FUNC(ELEMENT, RIN, NTURN, NELEMENT) 
+%       and are allowed to modify the particle coordinates
+%  
+%  ROUT=ringpass(...,'Silent') does not output the particle coordinates at
+%    each turn but only at the end of the tracking
+
 %
-% ROUT=RINGPASS(...,'KeepLattice') Tracking with the 'KeepLattice' flag is
-%   more efficient because it reuses persistent data structures stored in
-%   memory in previous calls to RINGPASS.
+%  INPUTS
+%  1. ring     - AT lattice
+%  2. RIN      - 6xN matrix: input coordinates of N particles
+%  3. NTURNS   - Number of turns to perform (default: 1)
 %
-%	!!! In order to use this option, RINGPASS must first be called
-%	without the 'KeepLattice' flag. It then assumes that the elements in RING
-%	DO NOT CHANGE between calls. Otherwise, RINGPASS  must be called again
+%  OPTIONAL
+%  NHIST       - number elements before the loss to be traced (default: 1)
+%  KeepLattice - Tracking with the 'KeepLattice' flag is more efficient because it reuses persistent data structures stored in
+%                memory in previous calls to ringpass.
+%
+%	!!! In order to use this option, ringpass must first be called
+%	without the 'KeepLattice' flag. It then assumes that the elements in ring
+%	DO NOT CHANGE between calls. Otherwise, ringpass  must be called again
 %   without 'KeepLattice'.
 %
-% ROUT=RINGPASS(...,'reuse') is kept for compatibilty with previous
-% versions. It has no effect.
+%   'reuse'    - no effect, kept for backwards compatibility
+%   'Silent'   - not output the particle coordinates at
+%                each turn but only at the end of the tracking
 %
-% ROUT=RINGPASS(...,'Silent') does not output the particle coordinates at
-%    each turn but only at the end of the tracking
+%  OUTPUTS
+%  1. ROUT     - 6x(N*NTURNS) matrix: output coordinates of N particles at
+%               the exit of each turn
+%  2. LOST     - 1xN logical vector, indicating lost particles
+%                If only one output is given, loss information is saved in
+%                global variable LOSSFLAG
+%  3. LASTTURN  - 1xN vector, last turn when loss occured
+%  4. LOSSINFO	- 1x1 structure with the following fields:
+%                 lost                 1xN logical vector, indicating lost particles
+%                 turn                 1xN vector, turn number where the particle is lost
+%                 element              1xN vector, element number where the particle is lost
+%                 coordinates_at_loss  6xN array, coordinates at the exit of
+%                                      the element where the particle is lost
+%                                      (sixth coordinate is inf if particle is lost in a physical aperture)
+%                 coordinates          6xNxNHIST array, coordinates at the entrance of the
+%                                      NHIST elements before the loss
 %
-% ROUT=RINGPASS(...,PREFUNC)
-% ROUT=RINGPASS(...,PREFUNC,POSTFUNC)
-% ROUT=RINGPASS(...,function_handle.empty,POSTFUNC)
-%   PREFUNC and POSTFUNC are function handles, PREFUNC is called
-%   immediately before tracking each element, POSTFUNC is called
-%   immediately after each element. Functions are called as:
-%
-%       ROUT=FUNC(ELEMENT, RIN, NTURN, NELEMENT)
-%
-%   and are allowed to modify the particle coordinates
-%
-% See also: LINEPASS
+% See Also linepass, atpass
+
+%%
 
 % Check input arguments
-if size(Rin,1)~=6
+if size(RIN,1)~=6
     error('Matrix of initial conditions, the second argument, must have 6 rows');
 end
 
 if nargin < 3
-    nturns = 1;
+    NTURNS = 1;
 end
-[keeplattice,args]=getflag(varargin, 'KeepLattice');
-[dummy,args]=getflag(args,'reuse');	%#ok<ASGLU> % Kept for compatibility and ignored
-[silent,args]=getflag(args, 'silent');
-funcargs=cellfun(@(arg) isa(arg,'function_handle'), args);
-nhist=getoption(struct(args{~funcargs}), 'nhist',1);
 
-newlattice = double(~keeplattice);
+% parsing input argument (flags)
+[KEEPLATTICEFLAG,args] = getflag(varargin, 'KeepLattice');
+[~,args]               = getflag(args,'reuse');	%#ok<ASGLU> % Kept for compatibility and ignored
+[SILENTFLAG,args]      = getflag(args, 'silent');
+funcargs               = cellfun(@(arg) isa(arg,'function_handle'), args);
+nhist                  = getoption(struct(args{~funcargs}), 'nhist',1);
 
-if silent
-    refpts=[];
+% Boolean
+NEWLATTICE = double(~KEEPLATTICEFLAG);
+
+if SILENTFLAG
+    refpts=[]; % Not output the particle coordinates
 else
-    refpts=length(ring)+1;
+    refpts = length(ring)+1; %otherwise output after each turn
 end
 
-[prefunc,postfunc]=parseargs({function_handle.empty,function_handle.empty},...
+%parsing input arguments
+[prefunc,postfunc] = parseargs({function_handle.empty,function_handle.empty},...
     args(funcargs));
 
 try
-    [Rout,lossinfo] = atpass(ring,Rin,newlattice,nturns,refpts,prefunc,postfunc,nhist);
-    
+    [ROUT,lossinfo] = atpass(ring, RIN, NEWLATTICE, NTURNS, refpts, prefunc, postfunc, nhist);    
     if nargout>1
-        if nargout>3, varargout{3}=lossinfo; end
+        if nargout>3, varargout{3} = lossinfo; end
         if nargout>2, varargout{2} = lossinfo.turn; end
         varargout{1} = lossinfo.lost;
     else % if no output arguments - create LOSSFLAG, for backward compatibility with AT 1.2
