757406f8c49d440e111cc1951fa49fdb5ffb0cf5
yysigari <64226663+yysigari@users.noreply.github.com>
2022-04-01 14:07:34 -0600

    Update linearoptics.m

--- Examples/linearoptics.m
+++ Examples/linearoptics.m
@@ -1,3 +1,20 @@
 % this example of calculating linear optics at e reference point
 
 M =linopt(THERING, 0, findcells(THERING, 'FamName' , 'SMbeam'));
+
+%LinData = 
+%
+%  struct with fields:
+
+ %     ElemIndex: 633
+  %         SPos: 128.1600
+   % ClosedOrbit: [4×1 double]
+    % Dispersion: [4×1 double]
+    %       M44: [4×4 double]
+     %     gamma: 0.9999
+     %        C: [2×2 double]
+      %        A: [2×2 double]
+       %       B: [2×2 double]
+       %    beta: [9.1314 2.6227]
+        %  alpha: [0.0013 -0.0036]
+         %    mu: [4.1853 1.3330]
