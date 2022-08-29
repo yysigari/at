68d3112852072e3a9e41a2dbe1d9d35286dfea16
yysigari <yysigari@users.noreply.github.com>
2022-08-29 16:21:13 +0000

    deploy: ba07c8db5e517a062ae830f7b371f745490f5789

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gb2ba30f',
+    VERSION: '0.1.dev1+gba07c8d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
